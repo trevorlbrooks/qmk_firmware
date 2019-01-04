924f02a611c147ad4113860de0c4bd393e550cf9
Trevor Brooks <tbrooks@mtech.edu>
2019-01-03 23:08:12 -0700

    Get some unicode from Drashna's layouts. Remove some LED controls.

--- layouts/community/ergodox/trevorlbrooks/keymap.c
+++ layouts/community/ergodox/trevorlbrooks/keymap.c
@@ -11,14 +11,18 @@ enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
   VRSN,
-  RGB_SLD
+  RGB_SLD,
+  UC_FLIP,           // (ಠ痊ಠ)┻━┻
+  UC_TABL,           // ┬─┬ノ( º _ ºノ)
+  UC_SHRG,           // ¯\_(ツ)_/¯
+  UC_DISA           // ಠ_ಠ
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |  F5  |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -40,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_F5,
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
@@ -49,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
+             KC_TRNS,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
              MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
@@ -72,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | EPRM  |      |      |      |      |                                       |   0  |   0  |   .  |   =  |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |Animat|      |       |Toggle|Solid |
+ *                                        |      |      |       |Toggle|Solid |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |Bright|Bright|      |       |      |Hue-  |Hue+  |
- *                                 |ness- |ness+ |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
+ *                                 |UC_   |UC_   |------|       |------|UC_   |UC_   |
+ *                                 |FLIP  |TABL  |      |       |      |SHRG  |DISA  |
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
@@ -87,18 +91,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
           EPRM,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       RGB_MOD,KC_TRNS,
+                                       KC_TRNS,KC_TRNS,
                                                KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+                               UC_FLIP,UC_TABL,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7, KC_F8, KC_F9,  KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,  KC_8,  KC_9,   KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,  KC_5,  KC_6,   KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,  KC_2,  KC_3,   KC_BSLS, KC_TRNS,
                          KC_0,  KC_0,  KC_DOT, KC_EQL,  KC_TRNS,
-       RGB_TOG, RGB_SLD,
+       RGB_TOG, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+       KC_TRNS, UC_SHRG, UC_DISA
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -188,6 +192,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+#ifdef UNICODE_ENABLE
+    case UC_FLIP: // (ノಠ痊ಠ)ノ彡┻━┻
+      if (record->event.pressed) {
+        send_unicode_hex_string("0028 30CE 0CA0 75CA 0CA0 0029 30CE 5F61 253B 2501 253B");
+      }
+      break;
+    case UC_TABL: // ┬─┬ノ( º _ ºノ)
+      if (record->event.pressed) {
+        send_unicode_hex_string("252C 2500 252C 30CE 0028 0020 00BA 0020 005F 0020 00BA 30CE 0029");
+      }
+      break;
+    case UC_SHRG: // ¯\_(ツ)_/¯
+      if (record->event.pressed) {
+        send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+      }
+      break;
+    case UC_DISA: // ಠ_ಠ
+      if (record->event.pressed) {
+        send_unicode_hex_string("0CA0 005F 0CA0");
+      }
+      break;
+#endif
   }
   return true;
 }
--- layouts/community/ergodox/trevorlbrooks/rules.mk
+++ layouts/community/ergodox/trevorlbrooks/rules.mk
@@ -1,1 +1,2 @@
 COMMAND_ENABLE = no
+UNICODE_ENABLE    = yes
